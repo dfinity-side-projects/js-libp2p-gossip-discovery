1e43044fcfdd6e8d7f248344e209bb1d63f4265b
wanderer <mjbecze@gmail.com>
2017-10-15 16:50:31 -0400

    init
    Signed-off-by: wanderer <mjbecze@gmail.com>

---
+++ index.js
@@ -0,0 +1,158 @@
+const Buffer = require('safe-buffer').Buffer
+const EventEmitter = require('events').EventEmitter
+const PeerInfo = require('peer-info')
+const PeerId = require('peer-id')
+const pull = require('pull-stream')
+const Pushable = require('pull-pushable')
+const Reader = require('pull-reader')
+const debug = require('debug')
+const log = debug('discovery:gossip')
+debug.enable('discovery:gossip')
+
+const PROTO = '/discovery/gossip/0.0.0'
+
+module.exports = class handlePeers extends EventEmitter {
+  /**
+   * @param {Object} node - an instace of [libp2p](https://github.com/libp2p/js-libp2p)
+   * @param {Number} targetNumberOfPeers - the max number of peers to add to the peer book
+   */
+  constructor (node, targetNumberOfPeers) {
+    super()
+    this.node = node
+    this.targetNumberOfPeers = targetNumberOfPeers
+  }
+
+  /**
+   * starts the gossip process
+   */
+  start () {
+    const node = this.node
+    node.handle(PROTO, (proto, conn) => {
+      const p = Pushable()
+      pull(p, conn)
+
+      let peers = peerBookToJson(node.peerBook)
+
+      if (Object.keys(peers).length === 0) {
+        p.push(Buffer.from([0]))
+      } else {
+        peers = Buffer.from(JSON.stringify(peers))
+        p.push(Buffer.from([peers.length]))
+        p.push(peers)
+      }
+      p.end()
+    })
+    this.peerDiscovery(this.targetNumberOfPeers)
+  }
+
+  /**
+   * stop discovery
+   */
+  stop () {
+    this.node.unhandle(PROTO)
+    this.node.removeListener('peer:connect', this._onConnection)
+  }
+
+  peerDiscovery (targetNumberOfPeers) {
+    const newPeers = this.node.peerBook.getAllArray()
+    this._peerDiscovery(this.node, targetNumberOfPeers, newPeers)
+    this.node.on('peer:connect', this._onConnection.bind(this))
+  }
+
+  _onConnection (peer) {
+    log('connected peer, restarting discovery')
+    try {
+      const info = this.node.peerBook.get(peer)
+      if (!info._askedForPeers) {
+        throw new Error()
+      }
+    } catch (e) {
+      this._peerDiscovery(this.node, this.targetNumberOfPeers, [peer])
+    }
+  }
+
+  _peerDiscovery (node, targetNumberOfPeers, newPeers) {
+    let knownPeers = node.peerBook.getAllArray()
+    if (knownPeers.length < targetNumberOfPeers && newPeers.length !== 0) {
+      newPeers.forEach(peer => {
+        peer._askedForPeers = true
+        node.dial(peer, PROTO, async (err, conn) => {
+          if (err) {
+            // remove peers that we cannot connect to
+            node.peerBook.remove(peer)
+          } else {
+            try {
+              const peers = await readPeers(node, conn)
+              const newPeers = await this.filterPeers(node, peers)
+              return this._peerDiscovery(node, targetNumberOfPeers, newPeers)
+            } catch (e) {
+              // remove peers that are potinal malicous
+              node.hangUp(peer, () => {
+                node.peerBook.remove(peer)
+                node.emit('error', peer)
+              })
+            }
+          }
+        })
+      })
+    }
+  }
+
+  filterPeers (node, peers) {
+    const ids = Object.keys(peers)
+    const newPeers = []
+    ids.forEach(async id => {
+      try {
+        node.peerBook.get(id)
+        log('already have peer ', id)
+      } catch (e) {
+        const peerId = PeerId.createFromB58String(id)
+        const peerInfo = new PeerInfo(peerId)
+        const addresses = peers[id]
+        addresses.forEach(ad => {
+          peerInfo.multiaddrs.add(`${ad}/ipfs/${id}`)
+        })
+        node.peerBook.put(peerInfo)
+        newPeers.push(peerInfo)
+        this.emit('peer', peerInfo)
+      }
+    })
+    return newPeers
+  }
+}
+
+function readPeers (node, conn) {
+  const reader = Reader()
+  pull(conn, reader)
+  return new Promise((resolve, reject) => {
+    reader.read(1, (err, len) => {
+      if (err) {
+        reject(err)
+      } else if (len[0] !== 0) {
+        reader.read(len[0], (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            data = data.toString()
+            const peers = JSON.parse(data)
+            reader.abort()
+            resolve(peers)
+          }
+        })
+      } else {
+        reader.abort()
+        resolve({})
+      }
+    })
+  })
+}
+
+function peerBookToJson (peerBook) {
+  let peers = {}
+  peerBook.getAllArray().forEach(pi => {
+    peers[pi.id.toB58String()] = pi.multiaddrs.toArray().map(add => {
+      return add.toString().split('/').slice(0, -2).join('/')
+    })
+  })
+  return peers
+}
---
+++ tests/index.js
@@ -0,0 +1,203 @@
+const tape = require('tape')
+const PeerInfo = require('peer-info')
+const PeerId = require('peer-id')
+const fs = require('fs')
+const pify = require('pify')
+const multiaddr = require('multiaddr')
+
+const libp2p = require('libp2p')
+const TCP = require('libp2p-tcp')
+const multiplex = require('libp2p-multiplex')
+const pull = require('pull-stream')
+
+const GossipDiscovery = require('../')
+
+class Node extends libp2p {
+  constructor (peerInfo, peerBook, options) {
+    options = options || {}
+
+    const modules = {
+      transport: [
+        new TCP()
+      ],
+      connection: {
+        muxer: [
+          multiplex
+        ]
+      }
+    }
+
+    super(modules, peerInfo, peerBook, options)
+    this.discovery = new GossipDiscovery(this, 3)
+  }
+}
+
+class Malicous extends libp2p {
+  constructor (peerInfo, peerBook, options) {
+    options = options || {}
+
+    const modules = {
+      transport: [
+        new TCP()
+      ],
+      connection: {
+        muxer: [
+          multiplex
+        ]
+      }
+    }
+
+    super(modules, peerInfo, peerBook, options)
+    this.handle('/discovery/gossip/0.0.0', (proto, conn) => {
+      pull(pull.empty(), conn)
+    })
+  }
+}
+
+class Malicous2 extends libp2p {
+  constructor (peerInfo, peerBook, options) {
+    options = options || {}
+
+    const modules = {
+      transport: [
+        new TCP()
+      ],
+      connection: {
+        muxer: [
+          multiplex
+        ]
+      }
+    }
+
+    super(modules, peerInfo, peerBook, options)
+    this.handle('/discovery/gossip/0.0.0', (proto, conn) => {
+      pull(pull.once(Buffer.from([99, 1])), conn)
+    })
+  }
+}
+
+tape('tests', async t => {
+  t.plan(3)
+  let count = 0
+
+  const peerIds = await Promise.all([0, 1, 2].map(id => getPeerId(id)))
+  const nodes = peerIds.map((pId, i) => {
+    const peerInfo = new PeerInfo(pId)
+    const node = new Node(peerInfo)
+
+    node.peerInfo.multiaddrs.add(`/ip4/127.0.0.1/tcp/909${i}`)
+    return node
+  })
+
+  await Promise.all(nodes.map(n => {
+    return new Promise((resolve, reject) => {
+      n.start(() => {
+        n.discovery.start()
+        resolve()
+      })
+    })
+  }))
+
+  const addresses = nodes.map(n => multiaddr(n.peerInfo.multiaddrs.toArray()[0]))
+  nodes[2].discovery.on('peer', peer => {
+    t.pass()
+    isDone()
+  })
+
+  nodes[1].discovery.on('peer', peer => {
+    t.pass()
+    isDone()
+  })
+
+  nodes[0].discovery.on('peer', peer => {
+    t.pass()
+    isDone()
+  })
+
+  await pify(nodes[0].dial.bind(nodes[0]))(addresses[1], '/discovery/gossip/0.0.0')
+  await pify(nodes[2].dial.bind(nodes[2]))(addresses[0], '/discovery/gossip/0.0.0')
+
+  function isDone () {
+    count++
+    if (count === 3) {
+      const stoping = nodes.map(n => {
+        return new Promise((resolve, reject) => {
+          n.discovery.stop()
+          n.stop(resolve)
+        })
+      })
+      Promise.all(stoping).then(() => {
+        t.end()
+      })
+    }
+  }
+})
+
+tape('erros - no data sent', async t => {
+  const peerIds = await Promise.all([0, 1].map(id => getPeerId(id)))
+  let peerInfo = new PeerInfo(peerIds[0])
+  let good = new Node(peerInfo)
+  good.peerInfo.multiaddrs.add(`/ip4/127.0.0.1/tcp/9090`)
+
+  peerInfo = new PeerInfo(peerIds[1])
+  const mal = new Malicous(peerInfo)
+  mal.peerInfo.multiaddrs.add(`/ip4/127.0.0.1/tcp/9091`)
+
+  await Promise.all([mal, good].map(n => {
+    return new Promise((resolve, reject) => {
+      n.start(() => {
+        if (n.discovery) {
+          n.discovery.start()
+        }
+        resolve()
+      })
+    })
+  }))
+  await pify(good.dial.bind(good))(mal.peerInfo.multiaddrs.toArray()[0], '/discovery/gossip/0.0.0')
+  good.on('error', () => {
+    t.equals(good.peerBook.getAllArray().length, 0)
+    good.stop(() => {})
+    mal.stop(() => {})
+    t.end()
+  })
+})
+
+tape('erros - invalid len', async t => {
+  const peerIds = await Promise.all([0, 1].map(id => getPeerId(id)))
+  let peerInfo = new PeerInfo(peerIds[0])
+  let good = new Node(peerInfo)
+  good.peerInfo.multiaddrs.add(`/ip4/127.0.0.1/tcp/9090`)
+
+  peerInfo = new PeerInfo(peerIds[1])
+  const mal = new Malicous2(peerInfo)
+  mal.peerInfo.multiaddrs.add(`/ip4/127.0.0.1/tcp/9091`)
+
+  await Promise.all([mal, good].map(n => {
+    return new Promise((resolve, reject) => {
+      n.start(() => {
+        if (n.discovery) {
+          n.discovery.start()
+        }
+        resolve()
+      })
+    })
+  }))
+  await pify(good.dial.bind(good))(mal.peerInfo.multiaddrs.toArray()[0], '/discovery/gossip/0.0.0')
+  good.on('error', () => {
+    t.equals(good.peerBook.getAllArray().length, 0)
+    good.stop(() => {})
+    mal.stop(() => {})
+    t.end()
+  })
+})
+
+async function getPeerId (index) {
+  let id
+  try {
+    id = pify(PeerId.createFromJSON)(require(`./id${index}`))
+  } catch (e) {
+    id = await pify(PeerId.create)()
+    fs.writeFileSync(`./id${index}.json`, JSON.stringify(id.toJSON()))
+  }
+  return id
+}
